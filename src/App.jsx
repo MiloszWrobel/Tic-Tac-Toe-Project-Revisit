01eb48dfd7cb93630d4359c4877675214e03716a
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-22 14:43:23 +0100

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <>
+      <h1>React Tic-Tac-Toe</h1>
+      <p>New Thing</p>
+    </>
+  );
 }
 
-export default App
+export default App;
