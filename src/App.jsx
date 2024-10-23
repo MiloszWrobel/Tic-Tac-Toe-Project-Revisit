28fd0c31992dd1ea52d510c2b9dd285ec086d840
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-23 11:43:13 +0100

    Created a player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Header from "./Header";
+import Player from "./Components/Player";
+
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+    </main>
   );
 }
 
--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  const image = "../public/game-logo.png";
-  return (
-    <>
-      <img src={image} />
-      <h1>React Ti-Tac-Toe</h1>
-    </>
-  );
-}
