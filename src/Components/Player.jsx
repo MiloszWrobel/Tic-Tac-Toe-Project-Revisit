383649d1dfa4a03be8ec8f7777791c8a6b3715db
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-23 11:52:09 +0100

    Updated the player component to have a working edit button

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing(() => !isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input></input>;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>edit</button>
+      <button on onClick={handleClick}>
+        edit
+      </button>
     </li>
   );
 }
