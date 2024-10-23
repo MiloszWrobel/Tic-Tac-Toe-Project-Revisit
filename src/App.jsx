da74a692295c16059a27045588d4eadf0f79b243
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-23 10:58:11 +0100

    Created seperate header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import Header from "./Header";
 function App() {
   return (
     <>
-      <h1>React Tic-Tac-Toe</h1>
-      <p>New Thing</p>
-      <p>New new test</p>
+      <Header />
     </>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  const image = "../public/game-logo.png";
+  return (
+    <>
+      <img src={image} />
+      <h1>React Ti-Tac-Toe</h1>
+    </>
+  );
+}
