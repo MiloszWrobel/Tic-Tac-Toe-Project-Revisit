30973cdb76189a0541e56b99338d4309cb4f6802
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-24 09:08:04 +0100

    Added functionality that highlights the currents player turn

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
         <GameBoard
           currentActivePlayer={activePlayer}
